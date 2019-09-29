beb0eaa00cbef51b3bc9a76bb68148747ba2bfa2
GabrielFCosta <gabr.cos@gmail.com>
2019-09-29 19:25:45 -0300

    método onLocationChanged do LocationListener implementado para atualizar variáveis de localização e habilitar botão da interface. Pressionar botão apresenta dados de localização na tela e desabilita o mesmo até a próxima atualização do listener.

--- app/src/main/java/ufba/seminario/matc89/geolocalizacao/MainActivity.java
+++ app/src/main/java/ufba/seminario/matc89/geolocalizacao/MainActivity.java
@@ -8,77 +8,85 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.Locale;
 import static android.location.Criteria.POWER_LOW;
 
-/*
-MainActivity precisa inmplementar interface LocationListener para métodos
-requestSingleUpdate() e requestLocationUpdates() aceitarem "this" como
-parâmetro.
- */
 public class MainActivity extends Activity implements LocationListener{
 
     private TextView latText;
     private TextView lngText;
     private TextView altText;
+    private Button press;
     private LocationManager locationMan;
     private String provider;
+
+    double lng;
+    double lat;
+    double alt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        latText = (TextView) findViewById(R.id.latitude);
-        lngText = (TextView) findViewById(R.id.longitude);
-        altText = (TextView) findViewById(R.id.altitude);
+        latText = findViewById(R.id.latitude);
+        lngText = findViewById(R.id.longitude);
+        altText = findViewById(R.id.altitude);
+        press = findViewById(R.id.button);
+        press.setText(R.string.wait);
+        press.setEnabled(false);
 
         try {
+
             locationMan = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
             Criteria criteria = new Criteria();
             criteria.setPowerRequirement(POWER_LOW);
+            criteria.setCostAllowed(false);
             criteria.setAltitudeRequired(true);
             provider = locationMan.getBestProvider(criteria,true);
 
-            locationMan.requestLocationUpdates(provider, 10000, 0, this);
+            locationMan.requestLocationUpdates(provider, 5000, 0, this);
             //locationMan.requestSingleUpdate(provider,this,null);
 
         } catch (SecurityException e) {
-            Toast.makeText(this,"no suitable permission is present.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"SecurityException", Toast.LENGTH_LONG ).show();
+
         } catch(IllegalArgumentException e){
-            Toast.makeText(this,"provider or listener is null or doesn't exist.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"Habilite permissão para localização e renicie aplicativo.", Toast.LENGTH_LONG).show();
         }
     }
+
     //Métodos da interface LocationListener.
-    public void onLocationChanged(Location location) {}
+    /*
+    Atualiza variáveis de latitude, longitude, altura e habilita botão
+    a cada atualização da localização.
+    */
+    public void onLocationChanged(Location location) {
+        lng = location.getLongitude();
+        lat = location.getLatitude();
+        alt =location.getAltitude();
+        press.setText(R.string.press);
+        press.setEnabled(true);
+
+    }
     public void onStatusChanged(String provider, int status, Bundle extras) {}
     public void onProviderEnabled(String provider) {}
     public void onProviderDisabled(String provider) {}
 
     //Método do botão
+    /*
+    Apresenta localização e desabilita botão (até a próxima atualização
+    da localização).
+     */
     public void ShowLocation(View v){
-        try {
-            Location location = locationMan.getLastKnownLocation (provider);
-
-            if(location != null) {
-                double lng = location.getLongitude();
-                double lat = location.getLatitude();
-                double alt =location.getAltitude();
-                latText.setText(String.format(Locale.getDefault(),"%f",lat));
-                lngText.setText(String.format(Locale.getDefault(), "%f", lng));
-                altText.setText(String.format(Locale.getDefault(), "%f", alt));
-            }
-            else
-                Toast.makeText(this,"nenhuma localização disponível.", Toast.LENGTH_SHORT).show();
-
-        } catch (SecurityException e) {
-            Toast.makeText(this,"no suitable permission is present.", Toast.LENGTH_SHORT).show();
-        } catch(IllegalArgumentException e){
-            Toast.makeText(this,"provider or listener is null or doesn't exist.", Toast.LENGTH_SHORT).show();
-        }
+        latText.setText(String.format(Locale.getDefault(),"%f",lat));
+        lngText.setText(String.format(Locale.getDefault(), "%f", lng));
+        altText.setText(String.format(Locale.getDefault(), "%f", alt));
+        press.setText(R.string.wait);
+        press.setEnabled(false);
     }
-
 
 }
